96895f12eaaa0edbc153ddcf26537c3cb63efafb
wr1472 <wadud.ruf@gmail.com>
2021-10-17 16:30:32 +0100

    Refactor common code to callable function.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {StackedBar} from './StackedBar';
 
 const updateInterval = 5000;
 const sum = (prev: number, curr: number) => prev + curr;
-const generateData = (_: unknown) => Math.floor(Math.random() * 100);
+const toGeneratedData = (_: unknown) => Math.floor(Math.random() * 100);
 const toPercentage = (value: number, _: unknown, values: number[]) => value / values.reduce(sum, 0) * 100;
 const toDatapoint = (value: number, index: number, values: number[]) => ({
     value,
@@ -14,6 +14,7 @@ const toDatapoint = (value: number, index: number, values: number[]) => ({
 });
 
 function App() {
+    const [loaded, setLoaded] = useState(false);
     const [data, setData] = useState([
         {value: 0, cumulativeValue: 0},
         {value: 0, cumulativeValue: 0},
@@ -21,21 +22,27 @@ function App() {
     ]);
 
     useEffect(() => {
-        const interval = setInterval(() =>
-            setData(data
-                .map(generateData)
-                .map(toPercentage)
-                .map(toDatapoint)), 
+        const interval = setInterval(() => {
+                setData(data
+                    .map(toGeneratedData)
+                    .map(toPercentage)
+                    .map(toDatapoint));
+                setLoaded(true);
+            }, 
             updateInterval);
     
         return () => clearInterval(interval);
-      }, [data]);
+      }, [data, loaded]);
 
     return <>
         <div className={css({display: "flex", justifyContent: "center", alignItems: "end", height: "calc(50vh)"})}>
-            <StackedBar data={data} />
+            {loaded ? 
+                <StackedBar data={data} /> :
+                <div className={css({fontWeight: 800})}>Loading Data...</div>}
         </div>
-        <div className={css({textAlign: "center", marginTop: 8, fontStyle: "italic"})}>d3 horizontal stacked bar</div>
+        <div className={css({textAlign: "center", marginTop: 8, fontStyle: "italic", color: "gray"})}>
+            d3 horizontal stacked bar
+        </div>
     </>;
 }
 
--- src/StackedBar.tsx
+++ src/StackedBar.tsx
@@ -7,31 +7,38 @@ const colors = [
     "lightsteelblue"
 ]
 
-export function StackedBar({data, width = 500}: {width?: number, data: {value: number, cumulativeValue: number}[]}) {
+export interface StackedBarDataPoint {
+    value: number, 
+    cumulativeValue: number
+};
+
+export function StackedBar({data, width = 500}: {width?: number, data: StackedBarDataPoint[]}) {
     const ref = useRef(null);
     const height = 10;
     
     useEffect(() => {   
         const scale = d3.scaleLinear([0, width]).domain([0, 100]);
-        const svg = d3.select(ref.current);        
+        const svg = d3.select(ref.current); 
+
+        const updateDimensions = (selection: any) => {
+            selection
+                .transition()
+                .duration(2000)
+                .attr('width', (d: StackedBarDataPoint) => scale(d.value))
+                .attr("x", (d: StackedBarDataPoint) => scale(d.cumulativeValue - d.value))
+        };
 
         const rects = svg
             .selectAll('rect')
             .data(data);
 
         rects.enter().append("rect")
-            .transition()
-            .duration(2000)
-            .attr('width', (d) => scale(d.value))
-            .attr("x", (d) => scale(d.cumulativeValue - d.value))
+            .call(updateDimensions)
             .attr('height', height)
             .attr('fill', (d, i) => colors[i]);
 
         rects
-            .transition()
-            .duration(2000)
-            .attr('width', (d) => scale(d.value))
-            .attr("x", (d) => scale(d.cumulativeValue - d.value));
+            .call(updateDimensions);
         
         rects.exit().remove();
     }, [ref, data, width]);
